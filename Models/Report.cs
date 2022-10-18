96d83895b161fa24a9025e5ac52ff5bdc257ef36
Klotske <ilya@mokhov.vg>
2022-10-18 10:43:12 +0000

    Add Report classes

---
+++ Models/Report.cs
@@ -0,0 +1,27 @@
+﻿namespace management_api.Models
+{
+    public class Report
+    {
+        public DateTime Start { get; set; }
+        
+        public DateTime End { get; set; }
+        
+        public List<ReportMonthItem> Months { get; set; } = new List<ReportMonthItem>();
+    }
+
+    public class ReportMonthItem
+    {
+        public DateTime Start { get; set; }
+        
+        public DateTime End { get; set; }
+
+        public List<ReportDepartmentItem> Departments { get; set; } = new List<ReportDepartmentItem>();
+    }
+
+    public class ReportDepartmentItem
+    {
+        public Department Department { get; set; }
+        
+        public int MonthTotal { get; set; }
+    }
+}
