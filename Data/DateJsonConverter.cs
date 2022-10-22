c967f20253e06b8afcd7791b81912831c79ff66a
Klotske <ilya@mokhov.vg>
2022-10-22 11:21:44 +0000

    DateTime formatter

---
+++ Data/DateJsonConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace management_api.Data
+{
+    public class DateJsonConverter : JsonConverter<DateTime>
+    {
+        private string _dateFormat = "yyyy-MM-dd";
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateTime.ParseExact(reader.GetString(), _dateFormat, CultureInfo.InvariantCulture);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(_dateFormat));
+        }
+    }
+}
